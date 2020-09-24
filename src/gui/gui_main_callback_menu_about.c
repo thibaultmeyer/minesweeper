55d3cea71670fa979054ce471a8cd5450375b7a4
Thibault Meyer <meyer.thibault@gmail.com>
2020-09-24 19:28:33 +0200

    Rewrite about dialog
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/gui/gui_main_callback_menu_about.c
+++ src/gui/gui_main_callback_menu_about.c
@@ -5,16 +5,20 @@
 void gui_main_callback_menu_about(GtkApplication *const app, gpointer window) {
     (void) app;        // Unused parameter
 
-    GdkPixbuf *const pixbuf = gui_image_load_from_memory_scale(gui_image_logo_bytes, gui_image_logo_length, 64, 64);
+    GdkPixbuf      *pixbuf    = gui_image_load_from_memory_scale(gui_image_logo_bytes, gui_image_logo_length, 64, 64);
+    GtkAboutDialog *gtk_about = GTK_ABOUT_DIALOG(gtk_about_dialog_new());
 
-    gtk_show_about_dialog(GTK_WINDOW(window),
-                          "comments", MINESWEEPER_DESCRIPTION,
-                          "copyright", MINESWEEPER_COPYRIGHT,
-                          "license_type", GTK_LICENSE_MIT_X11,
-                          "logo", pixbuf,
-                          "program_name", MINESWEEPER_APPNAME,
-                          "version", MINESWEEPER_VERSION,
-                          "website", MINESWEEPER_WEBSITE,
-                          NULL);
+    gtk_about_dialog_set_comments(gtk_about, MINESWEEPER_DESCRIPTION);
+    gtk_about_dialog_set_copyright(gtk_about, MINESWEEPER_COPYRIGHT);
+    gtk_about_dialog_set_license_type(gtk_about, GTK_LICENSE_MIT_X11);
+    gtk_about_dialog_set_logo(gtk_about, pixbuf);
+    gtk_about_dialog_set_program_name(gtk_about, MINESWEEPER_APPNAME);
+    gtk_about_dialog_set_version(gtk_about, MINESWEEPER_VERSION);
+    gtk_about_dialog_set_website(gtk_about, MINESWEEPER_WEBSITE);
+
+    gtk_window_set_transient_for(GTK_WINDOW(gtk_about), GTK_WINDOW(window));
+    gtk_dialog_run(GTK_DIALOG(gtk_about));
+
     g_object_unref(pixbuf);
+    g_object_unref(gtk_about);
 }
