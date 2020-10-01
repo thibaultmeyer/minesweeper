3bb1032807f0593e49cf6f5ea3cea607663be736
Thibault Meyer <meyer.thibault@gmail.com>
2020-10-01 22:53:09 +0200

    Reset timer on new game
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/gui/gui_main_callback_menu_new_game.c
+++ src/gui/gui_main_callback_menu_new_game.c
@@ -18,6 +18,7 @@ void gui_main_callback_menu_new_game(GtkButton *button, e_game_difficulty game_d
 
     // Clean
     if (gl_context.minesweeper_game != NULL) {
+        minesweeper_change_game_state(gl_context.minesweeper_game, GAME_STATE_GAME_OVER);
         minesweeper_destroy(gl_context.minesweeper_game);
 
         GList      *childrens = gtk_container_get_children(GTK_CONTAINER(gl_context.gtk_grid_game));
